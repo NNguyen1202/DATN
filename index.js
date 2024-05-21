5d838aed6be1ffc8298c6a0b1b12a15182db6c19
NNguyen1202 <vopvipbmt@gmail.com>
2024-05-21 09:27:26 +0200

    Update

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+
 const dotenv = require("dotenv").config();
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 dbConnect();
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
